d1c6261433d6984af3baf471ae7fbbb2cab3a5b7
Michael Bartlett <bartlettmic@gmail.com>
2020-09-24 14:25:17 -0600

    more informative splash text

--- ui/splash.go
+++ ui/splash.go
@@ -15,7 +15,8 @@ func NewSplashPanel(ui *UI) *SplashPanel {
 
 func (m *SplashPanel) initialize() {
 	logo := MustImageFromFile("octoprint-logo.png")
-	m.Label = MustLabel("Connecting to OctoPrint...")
+	m.Label = MustLabel("OctoPrint is waiting for printer connection...")
+	
 
 	box := MustBox(gtk.ORIENTATION_VERTICAL, 15)
 	box.SetVAlign(gtk.ALIGN_CENTER)
